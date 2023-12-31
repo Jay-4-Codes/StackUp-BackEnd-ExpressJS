8259d1b990e4f5490040005ac08e8aaaadbf66f4
Jaycodes <155115171+Jay-4-Codes@users.noreply.github.com>
2023-12-31 05:45:13 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
-// Import required modules
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-// Create an Express app
 const app = express();
-const port = 3000; // Change this to your desired port
+const port = 3000;
+
+// Configure EJS as the view engine
+app.set('view engine', 'ejs');
 
 // Middleware
 app.use(morgan('dev')); // Logging middleware
 app.use(bodyParser.json()); // JSON parsing middleware
 
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
 // Define routes
 app.get('/', (req, res) => {
-    res.status(200).send({
-        message: 'Hello World from ExpressJS!'
-    })
+res.render('index',{
+	time: new Date().toString()
+});
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
 });
 
 // Start the server
